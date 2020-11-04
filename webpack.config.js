6f663a5350502326797e40d6d154b83d6b4ef496
Mat Jones <mjones@andculture.com>
2020-11-04 15:19:16 -0500

    debugging global distribution

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,54 @@
 const path = require("path");
+const fs = require("fs");
 
-module.exports = {
-    entry: path.resolve(__dirname, "src/index.ts"),
-    mode: "production",
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
+const currentWorkingDirectory = __dirname;
+
+const configPromise = new Promise(function(resolve, reject) {
+    // read the webpack dependency externals config from a separate JSON file
+    fs.readFile("dependency-externals.json", "utf8", (err, externalsData) => {
+        if (err) {
+            reject(
+                `Unable to read './dependency-externals.json' file.  Reason: ${err}`
+            );
+        }
+
+        let externalsConfig = {};
+        try {
+            externalsConfig = JSON.parse(externalsData);
+        } catch (e) {
+            reject(
+                `Unable to convert './dependency-externals.json' file data to JSON object.  Reason: ${e}`
+            );
+        }
+
+        const webpackConfig = {
+            entry: path.resolve(currentWorkingDirectory, "src/index.ts"),
+            mode: "production",
+            module: {
+                rules: [
+                    {
+                        test: /\.tsx?$/,
+                        use: "ts-loader",
+                        exclude: /node_modules/,
+                    },
+                ],
             },
-        ],
-    },
-    externals: {
-        quill: "quill",
-    },
-    output: {
-        path: path.resolve(__dirname, "dist-global"),
-        filename: "index.js",
-        library: "AndcultureCodeQuill",
-        libraryTarget: "umd",
-    },
-    resolve: {
-        extensions: [".ts", ".js"],
-    },
-};
+            output: {
+                path: path.resolve(__dirname, "dist-global"),
+                filename: "index.js",
+                library: "AndcultureCodeQuill",
+                libraryTarget: "umd",
+            },
+            resolve: {
+                extensions: [".ts", ".js"],
+            },
+        };
+
+        // add the externals config to the webpack config
+        Object.assign(webpackConfig, externalsConfig);
+
+        resolve(webpackConfig);
+    });
+});
+
+module.exports = configPromise;
